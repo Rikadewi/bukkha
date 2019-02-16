ad786abfdac27e7660042ca3ea484858b1d5d7cf
rikadewi <rika_4444@yahoo.com>
2019-02-17 02:31:00 +0700

    Initialize coming soon page

---
+++ resources/views/coming_soon.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Bukkha</title>
+        <link rel="icon" type="image/ico" href="img/logo.png" />
+    </head>
+    <body>
+        <p>Coming Soon</p>
+    </body>
+</html>
